c98940f0569b8c108b0a9dcc4f6e11a6b732550b
jacksonnerdcamp <jacksonjjacobs@gmail.com>
2020-01-11 15:01:44 -0500

    working on pawn object

--- test.py
+++ test.py
@@ -1,6 +1,5 @@
 from graphics import *
 
-
 # step 1: set up board
 board = GraphWin('Board', 400, 400)
 board.setBackground('white')
@@ -9,14 +8,23 @@ tilex = []
 tiley = []
 for y in range(9):
     for x in range(9):
-        tilex.append(Rectangle(Point(x*50, y*50), Point(x*50 + 50, y*50 + 50)))
+        tilex.append(Rectangle(Point(x * 50, y * 50), Point(x * 50 + 50, y * 50 + 50)))
         tilex[x].draw(board)
-        if (x + y)%2 == 1:
+        if (x + y) % 2 == 1:
             tilex[x].setFill('black')
     tiley.append(tilex)
     tilex.clear()
 
 board.getMouse()
 
-class pawn:
-    sprite = Image(Point(0, 0), "")
+
+class Pawn:
+    color = 'white'
+    moves = [1, 0]
+    pawn = GraphicsObject()
+    def pawn(self, ):
+    Rectangle(Point(0, 20), Point(10, 40)).draw(pawn)
+    Oval(Point(0, 0), Point(10, 0)).draw(pawn)
+
+
+board.draw(Pawn.pawn)
